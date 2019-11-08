359e946eaf83d66f45220d0ac846219535c40432
igal-ra <igal.ratiner@logz.io>
2019-11-08 22:36:20 +0200

    refactoring from run to entrypoint in dockerfile

--- Computer/src/main/java/Main.java
+++ Computer/src/main/java/Main.java
@@ -1,5 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world from Computer");
+        while(true) {
+            try {
+                //sending the actual Thread of execution to sleep X milliseconds
+                Thread.sleep(3000);
+            } catch(InterruptedException ie) {}
+            System.out.println("Hello from Computer!");
+        }
     }
 }
