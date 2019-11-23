d3f767e909c32d5a62a4c21c6356048493896ea1
igal-ra <igal.ratiner@logz.io>
2019-11-23 10:58:02 +0200

    migrate to guice

--- Server/src/main/kotlin/rest/ServerResource.kt
+++ Server/src/main/kotlin/rest/ServerResource.kt
@@ -1,55 +1,33 @@
 package rest
 
 import client.Bla
-import di.serviceModule
+import com.google.inject.Inject
 import io.ktor.application.Application
 import io.ktor.application.call
-import io.ktor.application.install
-import io.ktor.features.CallLogging
-import io.ktor.features.ContentNegotiation
-import io.ktor.features.DefaultHeaders
-import io.ktor.gson.gson
 import io.ktor.request.receive
 import io.ktor.response.respond
 import io.ktor.routing.get
 import io.ktor.routing.post
 import io.ktor.routing.routing
-import org.koin.Logger.slf4jLogger
-import org.koin.core.KoinApplication.Companion.logger
-import org.koin.ktor.ext.Koin
-import org.koin.ktor.ext.inject
+import mu.KotlinLogging
+
 import service.ServerService
-import java.text.DateFormat
 
-fun Application.module() {
-    // Install Ktor features
-    install(DefaultHeaders)
-    install(CallLogging)
-    install(Koin) {
-        slf4jLogger()
-        modules(serviceModule)
-    }
-    install(ContentNegotiation) {
-        gson {
-            setDateFormat(DateFormat.LONG)
-            setPrettyPrinting()
-        }
-    }
 
-// Lazy inject HelloService
-    val service by inject<ServerService>()
-
-// Routing section
-    routing {
-        get("/hello") {
-            logger.info("Return back a message from Server: ${service.sayHello()}")
-            call.respond(Bla("Server", service.sayHello()))
-        }
-        post("/hello") {
-            val messageReceived : Bla = call.receive()
-            logger.info("A message from ${messageReceived.holder}: ${messageReceived.message}")
-            logger.info("Return back a message from Server: ${service.sayHello()}")
-            call.respond(Bla("Server", service.sayHello()))
+class ServerResource @Inject constructor(application: Application, service: ServerService)  {
+    init {
+        val logger = KotlinLogging.logger {}
+        application.routing {
+            get("/hello") {
+                logger.info("Return back a message from Server: ${service.sayHello()}")
+                call.respond(Bla("Server", service.sayHello()))
+            }
+            post("/hello") {
+                val messageReceived: Bla = call.receive()
+                logger.info("A message from ${messageReceived.holder}: ${messageReceived.message}")
+                logger.info("Return back a message from Server: ${service.sayHello()}")
+                call.respond(Bla("Server", service.sayHello()))
+            }
         }
     }
 }
